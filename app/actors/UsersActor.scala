d40a814e072128699262bcb70d59acdbc2679451
aelmod <aeldroid@gmail.com>
2017-11-05 22:27:53 +0200

    Covered by unit test action on UsersActor:
     - remove
     - get by username

--- app/actors/UsersActor.scala
+++ app/actors/UsersActor.scala
@@ -9,34 +9,46 @@ class UsersActor extends Actor {
 
   def behaviour(users: List[User]): Actor.Receive = {
     case GetList => sender() ! users
+
     case GetByUsername(username) => sender() ! users.find(_.username == username).map(FoundUser).getOrElse(UserNotFoundError)
-    case Add(username, lastName, firstName) => {
+
+    case Add(username, lastName, firstName) =>
       context.become(behaviour(users :+ User(username, lastName, firstName)))
       sender() ! UserSuccessfullyCreated
-    }
-    case Remove(username) => {
+
+    case Remove(username) =>
       users.find(_.username == username) match {
-        case Some(u) => context.become(behaviour(users.filterNot(_ == u)))
+        case Some(u) =>
+          context.become(behaviour(users.filterNot(_ == u)))
+          sender() ! UserSuccessfullyRemoved
         case None => sender() ! UserNotFoundError
       }
-    }
   }
 
   override def receive = behaviour(Nil)
 }
 
 object UsersActor {
+
   case class Add(username: String, lastName: String, firstName: String)
+
   case class Remove(username: String)
+
   case class GetByUsername(username: String)
+
   case object GetList
 
 
   sealed abstract class UsersMessage
 
   case class FoundUser(user: User) extends UsersMessage
+
   case object UserSuccessfullyCreated extends UsersMessage
 
+  case object UserSuccessfullyRemoved extends UsersMessage
+
   case object UserNotFoundError extends UsersMessage
+
   case object UserAlreadyExistError extends UsersMessage
+
 }
--- test/actors/UsersActorSpec.scala
+++ test/actors/UsersActorSpec.scala
@@ -1,6 +1,6 @@
 package actors
 
-import actors.UsersActor.{Add, GetList, UserSuccessfullyCreated}
+import actors.UsersActor._
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest._
@@ -45,8 +45,52 @@ class UsersActorSpec
 
       val users = expectMsgType[List[User]]
 
-      users.length should be (1)
-      users.head.username should be ("vader")
+      users.length should be(1)
+      users.head.username should be("vader")
+    }
+
+    "remove user successful" in {
+      usersActor ! Add("vader", "Zhuma", "Vova")
+      usersActor ! Add("freefly", "Melnik", "Sasha")
+      usersActor ! Add("bosgalo", "Igor", "Vova")
+      usersActor ! Add("aelmod", "Evgeniy", "Scherbyna")
+
+      usersActor ! Remove("vader")
+
+      usersActor ! GetList
+
+      fishForSpecificMessage() {
+        case UserSuccessfullyRemoved => ()
+      }
+
+      fishForSpecificMessage() {
+        case users: List[_] => users.length should be(3)
+      }
+    }
+
+    "remove user failure" in {
+      usersActor ! Add("freefly", "Melnik", "Sasha")
+
+      usersActor ! Remove("vader")
+
+      usersActor ! GetList
+
+      fishForSpecificMessage() {
+        case UserNotFoundError => ()
+      }
+
+      fishForSpecificMessage() {
+        case users: List[_] => users.length should be(1)
+      }
+    }
+
+    "find user by username successful" in {
+      usersActor ! Add("vader", "Melnik", "Sasha")
+      usersActor ! GetByUsername("vader")
+
+      fishForSpecificMessage() {
+        case FoundUser(u) => u.username should be("vader")
+      }
     }
   }
 
